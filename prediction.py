a6d1410fc20b17138ab6f9a987ba50231cbc26eb
QiJiang <77928612+QiJiang-QJ@users.noreply.github.com>
2025-01-24 20:06:28 +0800

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -62,37 +62,3 @@ def predict(args, ts, n_cells=None, ckpt_name='best'):
 
 
 
-def predict_Endes(args, ckpt_name='best'):
-
-    model, latent_listAllT, cell_types_listAllT, config = derive_model(args, ckpt_name=ckpt_name)
-
-    Endes_latent_allT = []
-    for ii, latent_xt in enumerate(latent_listAllT):
-        latent_xt = latent_xt.to(config.device)
-        latent_xt_endes = latent_xt + model.drift(None,latent_xt)
-        Endes_latent_allT.append(latent_xt_endes.detach().cpu().numpy())
-        
-    return latent_listAllT, Endes_latent_allT
-
-
-
-def predict_TrajectoryPoints(args, n_cells=2000, ckpt_name='final', dt = 0.1):
-    model, latent_listAllT, cell_types_listAllT, config = derive_model(args, ckpt_name=ckpt_name)
-
-    times = np.round(np.arange(0, len(latent_listAllT)-1+dt, dt), 3).tolist()
-    times = [np.float64(t) for t in times]
-
-    latent_xs_predict = model.predict(times, latent_listAllT[0], n_cells=n_cells)
-    
-    latent_xs_predict = [
-        item.detach().cpu().numpy() if isinstance(item, torch.Tensor) else item
-        for item in latent_xs_predict
-    ]
-
-    
-    return latent_listAllT, latent_xs_predict, times
-
-
-
-
-
